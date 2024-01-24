0145279aa9a808277861e864f748f540f7def7d2
narasimham321 <143310046+narasimham321@users.noreply.github.com>
2024-01-24 23:13:51 +0530

    first commit

---
+++ direction2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class direction2{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int n=a;
+        int k=0;
+        int p=1;
+        while(n!=0){
+            k=n%10;
+            p=p*k;
+            n=n/10;
+        }
+        System.out.println(p);
+    }
+    }
